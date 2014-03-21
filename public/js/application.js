8e56e5618eef13350ad1100c330d997ab407d6b2
jluyke <jordanluyke@gmail.com>
2014-03-21 16:49:31 -0700

    ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   surveyApp = new SurveyApp();
   surveyApp.addListeners();
+  // $("#login_bar").
 });
 
 var SurveyApp = function() {};
@@ -8,8 +9,8 @@ var SurveyApp = function() {};
 SurveyApp.prototype = {
   addListeners: function() {
     $('form[name="create_account"]').submit(surveyApp.clickSubmitSignup);
-    $('form[name="login"]').submit(surveyApp.clickSubmitSignup);
-    $('#logout').click(clickSubmitLogout);
+    $('form[name="login"]').submit(surveyApp.clickSubmitLogin);
+    $('#logout_bar').click(surveyApp.clickSubmitLogout);
   },
 
   clickSubmitSignup: function(event) {
@@ -21,6 +22,7 @@ SurveyApp.prototype = {
       data: formData
     }).done(function(data) {
       $("#login_bar").hide("slow");
+      $("#logout_bar").show("slow");
     }).fail(function(data, errorMsg) {
       alert("Signup Error");
     });
@@ -35,17 +37,18 @@ SurveyApp.prototype = {
       data: formData
     }).done(function(data) {
       $("#login_bar").hide("slow");
+      $("#logout_bar").show("slow");
     }).fail(function(data, errorMsg) {
       alert("Login Error");
     });
   },
 
-  clickSubmitLogout: function() {
+  clickSubmitLogout: function(event) {
     $.ajax({
       url: "/sessions",
       type: "delete"
     }).done(function(data) {
-      $("#logout").hide();
+      $("#logout_bar").hide("slow");
       $("#login_bar").show("slow");
     });
   }
