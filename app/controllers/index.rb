5f22c8c331a270e1aac2b4ba3a948c2476f3d7d9
Zohar Liran <zoharliran@gmail.com>
2014-03-21 13:20:06 -0700

    routes almost ready. to discuss with Jordan

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,16 +16,35 @@ post '/users' do #create a new user
   @user.save!
 end
 
-get '/surveys' do #home page
+get '/users/:id/surveys' do
+  current_user = User.find(params[:id])
+  @surveys = current_user.surveys
+  erb :surveys
+end
 
+get '/surveys' do #home page
+  @surveys = Survey.all
+  erb :surveys
 end
 
 get '/surveys/:id' do #take specific survey
-
+  @survey = Survey.find(params[:id])
+  @questions = Survey.questions
+  erb :survey
 end
 
 post '/surveys/:id' do #post data from specific survey
-
+  answers = params[:answers] #{question_id=>answer, 2=>answer, etc...}
+  #first user (id=1) is 'anonymous' 
+  user_id = session[:user_id] ? session[:user_id] : 1
+  results = {}
+  answers.each_pair{|key,value|
+    current_question = Question.find(key)
+    current_question.choices.create(user_id: user_id, choice: value)
+    results[:current_question.id] = current_question.answers_distribution
+  }
+  session[:results] = results
+  redirect "/surveys/#{params[:id]}/results"
 end
 
 get '/surveys/:id/results' do 
@@ -34,16 +53,25 @@ end
 
 get '/surveys/new' do #go to the page to create a new survey
 
+  erb :create_survey
 end
 
+#pass params[:questions] that will be {1=>"the first question", 2=>"second question", etc}
 post '/surveys' do #create the new survey 
-
+  current_user = User.find(session[:user_id])
+  current_survey = current_user.surveys.create(params[:survey_title])
+  params[:questions].each_value{|value|
+    current_survey.questions.create(content: value)
+  }
+  redirect "/users/#{current_user.id}/surveys"
 end
 
 get '/surveys/:id/edit' do #edit a specific survey
-
+  @survey = Survey.find(params[:id])
+  @questions = @survey.questions
+  erb :edit_survey
 end
 
-post '/surveys' do #Goes back to main surveys
-
+put '/surveys' do #Goes back to main surveys UPDATE Survey
+  
 end
